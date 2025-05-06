fc3e368ebb0541191ea2ec3b9d90753ecfb18b9d
gayithrishn <gayithrishn@gmail.com>
2025-05-06 17:43:44 +0530

    first commit

---
+++ src/main/java/com/ibm/App.java
@@ -0,0 +1,15 @@
+package com.ibm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("wlcom to  devops lab");
+        
+    }
+}
